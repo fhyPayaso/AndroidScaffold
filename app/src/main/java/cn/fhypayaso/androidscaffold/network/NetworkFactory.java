052d89842fd801a022c97cc2042a8c263ffbfb0a
fhyPayaso <410619823@qq.com>
2018-06-03 19:49:11 +0800

    change

--- app/src/main/java/cn/fhypayaso/androidscaffold/network/NetworkFactory.java
+++ app/src/main/java/cn/fhypayaso/androidscaffold/network/NetworkFactory.java
@@ -3,7 +3,6 @@ package cn.fhypayaso.androidscaffold.network;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import cn.fhypayaso.androidscaffold.App;
 import cn.fhypayaso.androidscaffold.common.CacheKey;
 import cn.fhypayaso.androidscaffold.common.Config;
 import cn.fhypayaso.androidscaffold.network.converter.ResponseConverterFactory;
@@ -14,7 +13,6 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 /**
  * @author FanHongyu.
