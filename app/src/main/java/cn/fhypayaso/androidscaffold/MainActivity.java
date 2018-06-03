5870e408d0a54d5a8490b79f93a2f936e26b04d7
fhyPayaso <410619823@qq.com>
2018-06-03 20:06:40 +0800

    删除

--- app/src/main/java/cn/fhypayaso/androidscaffold/MainActivity.java
+++ app/src/main/java/cn/fhypayaso/androidscaffold/MainActivity.java
@@ -1,57 +1,15 @@
 package cn.fhypayaso.androidscaffold;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TableLayout;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import cn.fhypayaso.androidscaffold.base.activity.BaseToolBarActivity;
-import cn.fhypayaso.androidscaffold.base.presenter.BaseContract;
-import cn.fhypayaso.androidscaffold.login.FragmentOne;
-import cn.fhypayaso.androidscaffold.login.MainPagerAdapter;
-import cn.fhypayaso.androidscaffold.utils.ThreadUtil;
-import cn.fhypayaso.androidscaffold.utils.ToastUtil;
-
-public class MainActivity extends BaseToolBarActivity {
-
-
-    private ViewPager mViewPager;
-    private TabLayout mTabLayout;
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected int getLayoutId() {
-        return R.layout.activity_main;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
-
-    @Override
-    protected void initData(Bundle savedInstanceState) {
-
-    }
-
-    @Override
-    protected void initView() {
-
-        mTabLayout = (TabLayout) findViewById(R.id.tab);
-        mViewPager = (ViewPager) findViewById(R.id.view_pager);
-        List<String> stringList = new ArrayList<>();
-        stringList.add("tab1");
-        stringList.add("tab2");
-
-        MainPagerAdapter adapter = new MainPagerAdapter(getSupportFragmentManager(), stringList);
-        mViewPager.setAdapter(adapter);
-        mTabLayout.setupWithViewPager(mViewPager);
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        App.getInstance().exitAppWithTwiceClick();
-    }
-
 }
